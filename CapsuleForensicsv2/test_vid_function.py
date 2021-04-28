6667a612216d80a9fb296ba411c5d0aba862aae0
log g <logg.com@gmail.com>
2021-04-28 20:25:56 +0800

    fix capsule

--- CapsuleForensicsv2/test_vid_function.py
+++ CapsuleForensicsv2/test_vid_function.py
@@ -211,6 +211,11 @@ def detect(vid):
     
     return cls, prob
 
+transform_fwd = transforms.Compose([
+    transforms.Resize((300, 10)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
 if __name__ == '__main__':
     import argparse
 
